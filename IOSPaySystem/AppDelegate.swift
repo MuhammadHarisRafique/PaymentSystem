48f6dd8b8e9c8cc7d701b94839e2d3295a7266eb
IOS Developer <haris.math@gmail.com>
2016-06-15 17:12:36 +0500

    adding braintree framework from cocoaPods

---
+++ IOSPaySystem/AppDelegate.swift
@@ -0,0 +1,52 @@
+//
+//  AppDelegate.swift
+//  IOSPaySystem
+//
+//  Created by IOS Developer on 14/06/2016.
+//  Copyright © 2016 Slash Global. All rights reserved.
+
+
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+    
+        return true
+        
+    }
+    
+
+    func applicationWillResignActive(application: UIApplication) {
+        
+        
+        
+        
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        
+        
+        
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+      
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        
+    }
+
+
+}
+
